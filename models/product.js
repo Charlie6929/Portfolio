8e18be5f14916ebec826dfc1aa97032f78433832
Charlie6929 <hostotosto@gmail.com>
2025-05-18 13:31:47 +0200

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,37 +1,4 @@
-/*
-const mongoose = require("mongoose");
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  url: { // Link del prodotto Amazon
-    type: String,
-    required: true,
-  },
-  currentPrice: {
-    type: Number,
-    required: true,
-  },
-  desiredPrice: { // Prezzo target per l'alert
-    type: Number,
-  },
-  lastChecked: {
-    type: Date,
-    default: Date.now,
-  },
-  inStock: {
-    type: Boolean,
-    default: true,
-  },
-  alertSent: {
-    type: Boolean,
-    default: false,
-  }
-});
 
-module.exports = mongoose.model("Product", productSchema);
-*/
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
@@ -69,4 +36,37 @@ const productSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Product", productSchema);
+/*
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: { // Link del prodotto Amazon
+    type: String,
+    required: true,
+  },
+  currentPrice: {
+    type: Number,
+    required: true,
+  },
+  desiredPrice: { // Prezzo target per l'alert
+    type: Number,
+  },
+  lastChecked: {
+    type: Date,
+    default: Date.now,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+  alertSent: {
+    type: Boolean,
+    default: false,
+  }
+});
 
+module.exports = mongoose.model("Product", productSchema);
+*/
