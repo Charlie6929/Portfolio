7caf9c09f2ba4f0696e00d4412bbd72f1cea9a7f
Charlie6929 <hostotosto@gmail.com>
2025-05-18 05:09:16 +0200

    Update product.js

--- models/product.js
+++ models/product.js
@@ -32,4 +32,41 @@ const productSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("Product", productSchema);
 */
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  url: { // Link del prodotto Amazon
+    type: String,
+    required: true,
+    trim: true
+  },
+  currentPrice: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  desiredPrice: { // Prezzo target per l'alert
+    type: Number,
+    min: 0
+  },
+  lastChecked: {
+    type: Date,
+    default: Date.now
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  alertSent: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model("Product", productSchema);
 
