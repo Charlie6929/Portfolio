adf4a556920e77e9b0923ef5af88816ed187d623
Charlie6929 <hostotosto@gmail.com>
2025-05-20 04:50:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -52,36 +52,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta ${PORT}`);
 });
-/*
-// server.js
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const PORT = process.env.PORT || 3000;
-require("dotenv").config();
-
-// Importa le rotte dei prodotti
-const productRoutes = require("./routes/productRoutes");
-
-// Middleware
-app.use(express.json());
-
-// Connessione a MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connesso a MongoDB Atlas"))
-  .catch((err) => console.error("Errore connessione MongoDB:", err));
-
-// Rotta base
-app.get("/", (req, res) => {
-  res.send("API attiva e funzionante!");
-});
-
-// Usa le rotte dei prodotti
-app.use("/api/products", productRoutes);
-
-// Avvio del server
-app.listen(PORT, () => {
-  console.log(`Server in ascolto sulla porta ${PORT}`);
-});
-*/
